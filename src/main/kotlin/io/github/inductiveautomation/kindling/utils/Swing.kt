56bce78853a8b78162bd9a8b8e9f28315897cb8b
Joshua Hansen <joshlha16@gmail.com>
2025-08-04 15:05:05 -0700

    Fix unresolved import due to merge conflict

--- src/main/kotlin/io/github/inductiveautomation/kindling/utils/Swing.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/utils/Swing.kt
@@ -2,17 +2,7 @@ package io.github.inductiveautomation.kindling.utils
 
 import com.formdev.flatlaf.extras.FlatSVGIcon
 import com.github.weisj.jsvg.SVGDocument
-import com.github.weisj.jsvg.attributes.ViewBox
 import com.github.weisj.jsvg.view.ViewBox
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.swing.Swing
-import org.jdesktop.swingx.decorator.AbstractHighlighter
-import org.jdesktop.swingx.decorator.ColorHighlighter
-import org.jdesktop.swingx.decorator.ComponentAdapter
-import org.jdesktop.swingx.decorator.Highlighter
-import org.jdesktop.swingx.prompt.BuddySupport
 import java.awt.Color
 import java.awt.Component
 import java.awt.Container
@@ -46,6 +36,15 @@ import javax.swing.event.DocumentListener
 import javax.swing.event.EventListenerList
 import javax.swing.text.Document
 import javax.swing.text.JTextComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.swing.Swing
+import org.jdesktop.swingx.decorator.AbstractHighlighter
+import org.jdesktop.swingx.decorator.ColorHighlighter
+import org.jdesktop.swingx.decorator.ComponentAdapter
+import org.jdesktop.swingx.decorator.Highlighter
+import org.jdesktop.swingx.prompt.BuddySupport
 
 /**
  * A common CoroutineScope bound to the event dispatch thread (see [Dispatchers.Swing]).
